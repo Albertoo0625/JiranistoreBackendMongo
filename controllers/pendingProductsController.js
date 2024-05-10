093d6864673670326d76a672cec9802711c07def
albertoo0625 <tooalbert79@gmail.com>
2024-05-10 12:06:08 +0300

    deploy

--- controllers/pendingProductsController.js
+++ controllers/pendingProductsController.js
@@ -1,3 +1,103 @@
+// const mongoose = require('mongoose');
+// const path = require('path');
+
+// // Assuming you have defined a Mongoose schema for pending products
+// const PendingProduct = require('../modal/pendingProductsSchema');
+
+// // 1. Add pending product
+// const addPendingProduct = async (req, res) => {
+//   if (!req.files) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+
+//   const { title, price, company, info, quantity, email, user_id } = req.body;
+//   const image = req.files.img;
+//   const cleanProductInfo = info.replace(/[\r\n\t]/g, ' ');
+
+//   const filepath = path.join(__dirname, '..', 'public', 'images', `${image.name}`);
+//   image.mv(filepath, (err) => {
+//     if (err) return res.status(500).json({ status: "error", message: err });
+//   });
+
+//   const fullFilePath = `https://jiranistorebackendmongo.onrender.com/images/${image.name}`;
+
+//   try {
+//     const newPendingProduct = new PendingProduct({
+//       pending_product_title: title,
+//       pending_product_img: fullFilePath,
+//       pending_product_price: price,
+//       pending_product_company: company,
+//       pending_product_info: cleanProductInfo,
+//       pending_product_quantity: quantity,
+//       pending_product_email: email,
+//       pending_product_user_id: user_id,
+//     });
+
+//     await newPendingProduct.save();
+//     return res.status(200).send("Form submitted successfully.");
+//   } catch (error) {
+//     return res.status(500).send(error.message);
+//   }
+// };
+
+// // 2. Get all pending products
+// const getAllPendingProducts = async (req, res) => {
+//   try {
+//     let products = await PendingProduct.find({ pending_product_approval_status: false });
+//     res.status(200).send(products);
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// };
+
+// // 3. Get single pending product
+// const getOnePendingProduct = async (req, res) => {
+//   let id = req.params.id;
+//   try {
+//     let product = await PendingProduct.findOne({ _id: id });
+//     res.status(200).send(product);
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// };
+
+// // 4. Update pending product
+// const updatePendingProduct = async (req, res) => {
+//   const id = req.params.id;
+//   const { approvalstatus } = req.body;
+//   try {
+//     let product = await PendingProduct.findOneAndUpdate(
+//       { _id: id },
+//       { pending_product_approval_status: approvalstatus },
+//       { new: true }
+//     );
+//     res.status(200).json(product);
+//   } catch (error) {
+//     res.status(500).json({ message: `Error updating product, ${error.message}` });
+//   }
+// };
+
+// // 5. Delete pending product by id
+// const deletePendingProduct = async (req, res) => {
+//   let id = req.params.id;
+//   try {
+//     await PendingProduct.findByIdAndDelete(id);
+//     res.status(200).send('Product is deleted!');
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// };
+
+// module.exports = {
+//   addPendingProduct,
+//   getAllPendingProducts,
+//   updatePendingProduct,
+//   getOnePendingProduct,
+//   deletePendingProduct
+// };
+
+
+
 const mongoose = require('mongoose');
 const path = require('path');
 
@@ -19,7 +119,7 @@ const addPendingProduct = async (req, res) => {
     if (err) return res.status(500).json({ status: "error", message: err });
   });
 
-  const fullFilePath = `https://jiranistorebackendmongo.onrender.com/images/${image.name}`;
+  const fullFilePath = `http://localhost:3500/images/${image.name}`;
 
   try {
     const newPendingProduct = new PendingProduct({
@@ -54,7 +154,7 @@ const getAllPendingProducts = async (req, res) => {
 const getOnePendingProduct = async (req, res) => {
   let id = req.params.id;
   try {
-    let product = await PendingProduct.findOne({ _id: id });
+    let product = await PendingProduct.findOne({ pending_product_id: id });
     res.status(200).send(product);
   } catch (error) {
     res.status(500).send(error.message);
@@ -67,7 +167,7 @@ const updatePendingProduct = async (req, res) => {
   const { approvalstatus } = req.body;
   try {
     let product = await PendingProduct.findOneAndUpdate(
-      { _id: id },
+      { pending_product_id: id },
       { pending_product_approval_status: approvalstatus },
       { new: true }
     );
@@ -81,7 +181,7 @@ const updatePendingProduct = async (req, res) => {
 const deletePendingProduct = async (req, res) => {
   let id = req.params.id;
   try {
-    await PendingProduct.findByIdAndDelete(id);
+    await PendingProduct.findOne({pending_product_id:id});
     res.status(200).send('Product is deleted!');
   } catch (error) {
     res.status(500).send(error.message);
--- modal/pendingProductsSchema.js
+++ modal/pendingProductsSchema.js
@@ -1,71 +1,176 @@
+// const mongoose = require('mongoose');
+
+// const pendingProductSchema = new mongoose.Schema({
+//     pending_product_id: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         auto: true,
+//     },
+//     pending_product_title: {
+//         type: String,
+//         required: true
+//     },
+//     pending_product_img: {
+//         type: String,
+//         required: true
+//     },
+//     pending_product_price: {
+//         type: Number,
+//         required: true
+//     },
+//     pending_product_company: {
+//         type: String,
+//         required: true
+//     },
+//     pending_product_info: {
+//         type: String,
+//         required: true,
+//         maxlength: 8000
+//     },
+//     pending_product_inCart: {
+//         type: Boolean,
+//         default: false,
+//         required: true
+//     },
+//     pending_product_count: {
+//         type: Number,
+//         default: 0,
+//         required: true
+//     },
+//     pending_product_total: {
+//         type: Number,
+//         default: 0,
+//         required: true
+//     },
+//     pending_product_quantity: {
+//         type: Number,
+//         default: 0,
+//         required: true
+//     },
+//     pending_product_email: {
+//         type: String,
+//         required: true
+//     },
+//     pending_product_createdAt: {
+//         type: Date,
+//         default: Date.now,
+//         required: true
+//     },
+//     pending_product_updatedAt: {
+//         type: Date,
+//         default: Date.now,
+//         required: true
+//     }
+// });
+
+// const PendingProduct = mongoose.model('PendingProduct', pendingProductSchema);
+
+// module.exports = PendingProduct;
+
+
+
+
+
+
 const mongoose = require('mongoose');
 
-const pendingProductSchema = new mongoose.Schema({
-    pending_product_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        auto: true,
-    },
-    pending_product_title: {
-        type: String,
-        required: true
-    },
-    pending_product_img: {
-        type: String,
-        required: true
-    },
-    pending_product_price: {
-        type: Number,
-        required: true
-    },
-    pending_product_company: {
-        type: String,
-        required: true
-    },
-    pending_product_info: {
-        type: String,
-        required: true,
-        maxlength: 8000
-    },
-    pending_product_inCart: {
-        type: Boolean,
-        default: false,
-        required: true
-    },
-    pending_product_count: {
-        type: Number,
-        default: 0,
-        required: true
-    },
-    pending_product_total: {
-        type: Number,
-        default: 0,
-        required: true
-    },
-    pending_product_quantity: {
-        type: Number,
-        default: 0,
-        required: true
-    },
-    pending_product_email: {
-        type: String,
-        required: true
-    },
-    pending_product_createdAt: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    pending_product_updatedAt: {
-        type: Date,
-        default: Date.now,
-        required: true
-    }
+// Create a schema for the counters collection
+const counterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 1 },
 });
 
+// Create a model for the counters collection
+const Counter = mongoose.model('PendingProductCounter', counterSchema);
+
+// Define the pending product schema
+const pendingProductSchema = new mongoose.Schema({
+  pending_product_id: {
+    type: Number,
+    unique: true, // Ensure uniqueness
+  },
+  pending_product_title: {
+    type: String,
+    required: true
+  },
+  pending_product_img: {
+    type: String,
+    required: true
+  },
+  pending_product_price: {
+    type: Number,
+    required: true
+  },
+  pending_product_company : {
+    type: String,
+    required: true
+  },
+  pending_product_info : {
+    type: String,
+    required: true
+  },
+  pending_product_inCart:{
+    type: Boolean,
+    default: false
+  },
+  pending_product_count:{
+    type: Number,
+    default: 0
+  },
+  pending_product_total:{
+    type: Number,
+    default: 0
+  },
+  pending_product_quantity:{
+    type: Number,
+    default: 0
+  },
+  pending_product_user_id:{
+    type: Number,
+    allowNull:true
+  },
+  pending_product_email:{
+    type: String,
+    required: true
+  },
+  pending_product_approval_status:{
+    type: Boolean,
+    default: false
+  },
+  pending_product_createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  pending_product_updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Middleware to auto-increment pending_product_id
+pendingProductSchema.pre('save', async function (next) {
+  if (!this.isNew) {
+    // Only run this middleware for new documents
+    return next();
+  }
+
+  try {
+    const counter = await Counter.findByIdAndUpdate(
+      { _id: 'pending_product_id' }, // Use a specific identifier for your counter
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+
+    this.pending_product_id = counter.seq;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Create the PendingProduct model
 const PendingProduct = mongoose.model('PendingProduct', pendingProductSchema);
 
 module.exports = PendingProduct;
-
 
 
 
